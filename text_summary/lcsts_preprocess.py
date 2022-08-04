11d00be25341b10d4724529a2566faac74e4c9c7
Diligence1989 <songcaifu07@163.com>
2022-08-04 14:31:55 +0800

    20220804

--- text_summary/lcsts_preprocess.py
+++ text_summary/lcsts_preprocess.py
@@ -1,23 +1,29 @@
-import os,sys
+import os
+import sys
 import json
 
-titles = []
-content = []
-combines = []
 
-with open('/home/songcaifu/kg/transformer_usage/text_summary/lcsts_tsv/valid.src.txt','r') as f:
-    contents = f.readlines()
-    contents = [i.replace('\n', '') for i in contents]
-    print(contents[0:2])
+def preprocess(src, tgt, savefile):
+    titles = []
+    content = []
+    combines = []
+    # with open('/home/songcaifu/kg/transformer_usage/text_summary/lcsts_tsv/valid.src.txt','r') as f:
+    with open(src, 'r') as f:
+        contents = f.readlines()
+        contents = [i.replace('\n', '') for i in contents]
 
-with open('/home/songcaifu/kg/transformer_usage/text_summary/lcsts_tsv/valid.tgt.txt','r') as f:
-    titles = f.readlines()
-    titles = [i.replace('\n', '') for i in titles]
-    print(titles[0:2])
+    # with open('/home/songcaifu/kg/transformer_usage/text_summary/lcsts_tsv/valid.tgt.txt','r') as f:
+    with open(tgt, 'r') as f:
+        titles = f.readlines()
+        titles = [i.replace('\n', '') for i in titles]
 
-for i, title in enumerate(titles):
-    combines.append(title + '!=!' + contents[i])
+    for i, title in enumerate(titles):
+        combines.append(title + '!=!' + contents[i]+'\n')
 
-#print(combines[-1])
-with open() as f:
-    f.writelines()
+    with open(savefile, 'w') as f:
+        f.writelines(combines)
+
+
+#preprocess('lcsts_tsv/train.src.txt', 'lcsts_tsv/train.tgt.txt', 'lcsts_tsv/data1.tsv')
+#preprocess('lcsts_tsv/valid.src.txt', 'lcsts_tsv/valid.tgt.txt', 'lcsts_tsv/data2.tsv')
+#preprocess('lcsts_tsv/test.src.txt', 'lcsts_tsv/test.tgt.txt', 'lcsts_tsv/data3.tsv')
